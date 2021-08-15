e9be377fab5f2e2fca6379b583ce051cc1816d2f
codenious9 <88280243+codenious9@users.noreply.github.com>
2021-08-15 17:45:16 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public boolean isPalindrome(int x) {
+        int org_num = x;
+        int revNum=0;
+        while(org_num>0){
+            int pop = org_num%10;
+            revNum= revNum*10 + pop;
+            org_num=+org_num/10;
+        }
+        
+        if(revNum==x) return true;
+        else return false;
+        
+        
+    }
+}
