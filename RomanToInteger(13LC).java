c275e0f22cbf3976c046742d357ee5a386063dc6
codenious9 <88280243+codenious9@users.noreply.github.com>
2021-08-21 03:02:07 +0530

    Create RomanToInteger(13LC).java

---
+++ RomanToInteger(13LC).java
@@ -0,0 +1,12 @@
+private static int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+private static int romanInt[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+Public string Integer2Roman(int num){
+     StringBuilder sb = new StringBuilder();
+  for(int i =0;i<values.length;i++){
+  while(num>=values[i]){
+    num -=values[i];
+    sb.append(romanInt[i]);
+  }
+  }
+  return sb.toString();
+}
